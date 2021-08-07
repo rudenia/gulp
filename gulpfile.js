63aee118ebb33396dc02eb429be29d735418e84a
rudenia <45589318+rudenia@users.noreply.github.com>
2021-08-07 13:03:06 +0200

    Add Babel, Uglify

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require('gulp')
 const gulpPug = require('gulp-pug')
 const sass = require('gulp-sass')(require('sass')); // подключаем sass
 const gulpPlumber = require('gulp-plumber') // позволяет серверу работать даже если есть ошибка в файлах
+const gulpBabel = require('gulp-babel') // оптимизирует js код для старых браузеров
+const gulpUglify = require('gulp-uglify') // добавляет минификацию js файлов
 const gulpAutoprefixer = require('gulp-autoprefixer') // добавляет префиксы для старых браузеров
 const gulpCleanCss = require('gulp-clean-css') // добавляет минификацию css файлов
 
@@ -26,4 +28,13 @@ function scss2css() {
         .pipe(gulp.dest('dist/static/css/'));
 }
 
-exports.default = gulp.series(pug2html, scss2css);
+function script() {
+    return gulp.src('dev/static/js/main.js')
+        .pipe(gulpBabel({
+            presets: ['@babel/env']
+        }))
+        .pipe(gulpUglify())
+        .pipe(gulp.dest('dist/static/js/'));
+}
+
+exports.default = gulp.series(pug2html, scss2css, script);
