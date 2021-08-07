2d5252f071a3ccfe60441f1c28aa2f02df7f39f4
rudenia <45589318+rudenia@users.noreply.github.com>
2021-08-07 12:47:29 +0200

    Add Sass, Autoprefixer and CleanCss

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 const gulp = require('gulp')
 const gulpPug = require('gulp-pug')
+const sass = require('gulp-sass')(require('sass')); // подключаем sass
 const gulpPlumber = require('gulp-plumber') // позволяет серверу работать даже если есть ошибка в файлах
+const gulpAutoprefixer = require('gulp-autoprefixer') // добавляет префиксы для старых браузеров
+const gulpCleanCss = require('gulp-clean-css') // добавляет минификацию css файлов
+
 
 function pug2html() {
     return gulp.src('dev/pug/pages/*.pug')
@@ -12,4 +16,14 @@ function pug2html() {
         .pipe(gulp.dest('dist'));
 }
 
-exports.default = gulp.series(pug2html);
+function scss2css() {
+    return gulp.src('dev/static/styles/styles.scss')
+        .pipe(gulpPlumber())
+        .pipe(sass())
+        .pipe(gulpAutoprefixer())
+        .pipe(gulpCleanCss())
+        .pipe(gulpPlumber.stop())
+        .pipe(gulp.dest('dist/static/css/'));
+}
+
+exports.default = gulp.series(pug2html, scss2css);
