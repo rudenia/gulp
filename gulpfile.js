e5d51e1e8934883278584c2a19370f92756a06a9
rudenia <45589318+rudenia@users.noreply.github.com>
2021-08-08 10:34:14 +0200

    Add Imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const sass = require('gulp-sass')(require('sass')); // подключаем sass
 const gulpPlumber = require('gulp-plumber') // позволяет серверу работать даже если есть ошибка в файлах
 const gulpBabel = require('gulp-babel') // оптимизирует js код для старых браузеров
 const gulpUglify = require('gulp-uglify') // добавляет минификацию js файлов
+const gulpImagemin = require('gulp-imagemin') // уменьшаем размер изображений
 const gulpAutoprefixer = require('gulp-autoprefixer') // добавляет префиксы для старых браузеров
 const gulpCleanCss = require('gulp-clean-css') // добавляет минификацию css файлов
 const browserSync = require('browser-sync').create() // добавляет минификацию css файлов
@@ -44,6 +45,24 @@ function script() {
         .pipe(gulp.dest('dist/static/js/'));
 }
 
+function imageMin() {
+    return gulp.src(
+        ['dev/static/images/**/*.{jpg,png,gif,svg}'],
+        ['!dev/static/images/sprite/*'])
+        .pipe(gulpImagemin([
+            gulpImagemin.gifsicle({interlaced: true}),
+            gulpImagemin.mozjpeg({quality: 75, progressive: true}),
+            gulpImagemin.optipng({optimizationLevel: 5}),
+            gulpImagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]))
+        .pipe(gulp.dest('dist/static/images/'));
+}
+
 function watch() {
     browserSync.init({
         server: {
@@ -53,8 +72,9 @@ function watch() {
 
     gulp.watch("dev/pug/**/*.pug", pug2html);
     gulp.watch("dev/static/styles/**/*.scss", scss2css);
+    gulp.watch("dev/static/images/**/*.{jpg,png,gif,svg}", imageMin);
     gulp.watch("dev/static/js/main.js", script);
     gulp.watch("dist/*.html").on('change', browserSync.reload);
 }
 
-exports.default = gulp.series(clean, pug2html, scss2css, script, watch);
+exports.default = gulp.series(clean, pug2html, scss2css, imageMin, script, watch);
